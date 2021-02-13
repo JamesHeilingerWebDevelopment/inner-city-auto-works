3170b9978963c2e06dc4012851a9ce49f998a364
TheOriginalSpaceMonkey <SpaceMonkeyLab@users.noreply.github.com>
2021-02-13 12:31:56 -0800

    Started using my portfolio page as a template. SVG images added, navbar working, sidenav working.

---
+++ js/sideNav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  // document.querySelector('.hamburger-menu').style.display = 'none';
+  document.querySelector('.side-nav').style.width = '200px';
+}
+
+function closeNav() {
+  document.querySelector('.side-nav').style.width = '0';
+  // document.querySelector('.hamburger-menu').style.display = 'block';
+}
